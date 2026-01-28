6a866780b535e17586dd5c7ecad1bc679eb440d0
kdmn0 <ardabis35@gmail.com>
2026-01-28 20:50:02 +0300

    Temel URL yönlendirme ve view yapısı eklendi

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def login_view(request):
+    return HttpResponse("Login Sayfası")
+
+def register_view(reques):
+    return HttpResponse("Register Sayfası")
